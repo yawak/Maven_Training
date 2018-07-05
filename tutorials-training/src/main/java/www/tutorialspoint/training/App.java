25edc542c93067fbc2d120008174a022e563655c
arnoldg3 <arnoldg3@verifone.com>
2018-07-05 21:27:25 +0800

    Initial tutorials point training creation. Learned Super Pom

---
+++ tutorials-training/src/main/java/www/tutorialspoint/training/App.java
@@ -0,0 +1,13 @@
+package www.tutorialspoint.training;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
